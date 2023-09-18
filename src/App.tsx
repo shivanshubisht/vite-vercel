66e3bb7652bbfe31d96fde93924737187ba0daae
shivanshu <bishtshivanshu@gmail.com>
2023-09-19 04:48:40 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+// import { use } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState('')
+  useEffect(() => {
+    fetch('/api/handler')
+      .then((res) => res.json())
+      .then((res) => setData(res))
+  })
+  // const data = use(fetch('/api/handler').then((res) => res.json()))
+  // console.log(data)
+
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>{data}</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
